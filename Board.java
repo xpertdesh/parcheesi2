f98d2b41077f2a98e286bf3a0b95190b0fa6dc94
Tyler Viacara <tyler.viacara@yahoo.com>
2020-06-07 23:19:38 +0000

    Tyler

--- Board.java
+++ Board.java
@@ -25,24 +25,26 @@ public class Board{
 
             for (int i = 0; i < players.size(); i++) {
                 Player current = players.get(i);
-                int roll = dice.rollDice();
+				if(winner == false){
+                	int roll = dice.rollDice();
 				
-				if(players.get(i).getPiece().getCanMove == false){
-					if (roll == 5){
-						System.out.println("Congratulations " + players.get(i).getName() + ", you can move forward with your next roll!");
-						players.get(i).getPiece().setCanMove(true);
+					if(players.get(i).getPiece().getCanMove == false){
+						if (roll == 5){
+							System.out.println("Congratulations " + players.get(i).getName() + ", you can move forward with your next roll!");
+							players.get(i).getPiece().setCanMove(true);
+						}
+						else{
+							System.out.println("Sorry but you need to roll a 5 before you can move");
+						}
 					}
 					else{
-						System.out.println("Sorry but you need to roll a 5 before you can move");
-					}
+						current.getPiece().setLocation(roll);
+                		current.getPiece().setSpacesTraveled(roll);
+						current.showState();
+                		//winner = current.getPiecesState();
+                		winner = current.madeIt();
+            		}
 				}
-				else{
-					current.getPiece().setLocation(roll);
-                	current.getPiece().setSpacesTraveled(roll);
-					current.showState();
-                	//winner = current.getPiecesState();
-                	winner = current.madeIt();
-            }
         }
     }
 }
