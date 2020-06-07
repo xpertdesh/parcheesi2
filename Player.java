dab1b77f92758e44b0eb0a59f6154b25e5ac6210
xpertdesh <rcabrera@mail.umw.edu>
2020-06-07 20:17:22 +0000

    new file:   Player.java

---
+++ Player.java
@@ -0,0 +1,25 @@
+import java.util.Arraylist;
+
+public class Player{
+
+    private String name;
+    private ArrayList<Piece> pieces;
+    
+    Player(String name, int place){
+        this.name = name;
+        pieces = new ArrayList<Piece>(3);
+        for(int i=0; i<pieces.size(); i++){
+            pieces.add(new Piece(place));
+        }
+    }
+
+    
+    void showState(){
+        System.out.println(name + "'s pieces:");
+        for(int i=0; i<pieces.size(); i++){
+            System.out.println("Piece " + (i+1) + " is at location " + pieces.get(i).getLocation());
+        }
+    }
+
+
+}
