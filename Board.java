dea02e0218bd2972632232860bb460737ea767e0
Tyler Viacara <tyler.viacara@yahoo.com>
2020-06-07 23:27:21 +0000

    Tyler

--- Board.java
+++ Board.java
@@ -2,33 +2,33 @@ import java.util.*;
 
 public class Board{
 
-    private ArrayList<Player> players;
-    private static Board theBoard;
-    public Board(){
+	private ArrayList<Player> players;
+	private static Board theBoard;
+	public Board(){
 
-    }
-    public static Board getInstance(){
-        if(theBoard == null){
-            theBoard = new Board();
-        }
-        return theBoard;
-    }
-    public void setPlayers(ArrayList<Player> players){
-        this.players = players;
-    }
+	}
+	public static Board getInstance(){
+		if(theBoard == null){
+			theBoard = new Board();
+		}
+		return theBoard;
+	}
+	public void setPlayers(ArrayList<Player> players){
+		this.players = players;
+	}
 
-    public void runGame(){
-        Dice dice = Dice.getInstance();
-        boolean winner = false;        
+	public void runGame(){
+		Dice dice = Dice.getInstance();
+		boolean winner = false;        
 
-        while (winner == false) {
+		while (winner == false) {
 
-            for (int i = 0; i < players.size(); i++) {
-                Player current = players.get(i);
+			for (int i = 0; i < players.size(); i++) {
+				Player current = players.get(i);
 				if(winner == false){
-                	int roll = dice.rollDice();
-				
-					if(players.get(i).getPiece().getCanMove == false){
+					int roll = dice.rollDice();
+
+					if(players.get(i).getPiece().getCanMove() == false){
 						if (roll == 5){
 							System.out.println("Congratulations " + players.get(i).getName() + ", you can move forward with your next roll!");
 							players.get(i).getPiece().setCanMove(true);
@@ -39,13 +39,13 @@ public class Board{
 					}
 					else{
 						current.getPiece().setLocation(roll);
-                		current.getPiece().setSpacesTraveled(roll);
+						current.getPiece().setSpacesTraveled(roll);
 						current.showState();
-                		//winner = current.getPiecesState();
-                		winner = current.madeIt();
-            		}
+						//winner = current.getPiecesState();
+						winner = current.madeIt();
+					}
 				}
-        }
-    }
+			}
+		}
+	}
 }
-
