23644a67f587c45039d0dea60050f32d2872054e
Zach Brennan <zach@Zachs-MacBook-Air.local>
2020-06-07 17:28:08 -0400

    Began making players move in Board.java

--- Board.java
+++ Board.java
@@ -16,5 +16,17 @@ public class Board{
 	public void setPlayers(ArrayList<Player> players){
 		this.players = players;
 	}
+	
+	/*boolean winner = false;
+	Dice dice = new Dice();
+	
+	while (winner != true) {
+		for (int i = 0; i < players.size(); i++) {
+			int roll = dice.rollDice();
+			players.get(i).setLocation(roll);
+			players.get(i).setSpacesTraveled(roll);
+		}
+	}
+	* */
 }
 
