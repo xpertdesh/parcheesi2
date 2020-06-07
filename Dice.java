f2e3d5327c028609d57498faa66241c2a469ea8e
xpertdesh <rcabrera@mail.umw.edu>
2020-06-07 20:57:09 +0000

    Dice added

---
+++ Dice.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class Dice{
+    private static Dice INSTANCE;
+    
+    private Dice(){}
+
+    public static Dice getInstance(){
+        if(INSTANCE == null)
+            INSTANCE = new Dice();
+        return INSTANCE;
+    }
+    
+    static int rollDice(){
+        Random random = new Random();
+        return random.nextInt(6);
+    }
+
+    
+}
