70afe038ca4d444b4f86842144d8b8af61730a7c
Tyler Viacara <tyler.viacara@yahoo.com>
2020-06-15 01:16:15 +0000

    just added stratergy to tyler.java

--- Tyler.java
+++ Tyler.java
@@ -9,6 +9,17 @@ public class Tyler extends Player{
 	}
 	@Override
 	public int movePiece(int roll, ArrayList<Integer> enemyList){
+		int newLocation = 0;
+		for (int i = 0; i < this.getPiecesList().size(); i++){
+			newLocation = (this.getPiecesList().get(i).getLocation()) + roll;
+			for (int j = 0; j < enemyList.size(); j++){
+				if (newLocation == enemyList.get(j)){
+					this.getPiece(i).setLocation(roll);
+					this.getPiece(i).setSpacesTraveled(roll);
+					return i;
+				}
+			}
+		}
 		int fartherstBack = 100;
 		int pieceToMove = 0;
 		for (int tyler = 0; tyler < this.getPiecesList().size(); tyler++){
